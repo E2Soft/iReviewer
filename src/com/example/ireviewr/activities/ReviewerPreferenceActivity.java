dbab2ddd4938836d8a0d4fd95a0b655aef87c069
Milos Simic <milossimicsimo@gmail.com>
2015-06-22 16:51:33 +0200

    [UPDATE] Recaiver sada podrzava i podesenja [NEW] dodate akticnosti i fajlovi koji podrzavaju podesenja

---
+++ src/com/example/ireviewr/activities/ReviewerPreferenceActivity.java
@@ -0,0 +1,41 @@
+package com.example.ireviewr.activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+import com.example.ireviewr.R;
+
+public class ReviewerPreferenceActivity extends PreferenceActivity {
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preferences);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    // Respond to the action bar's Up/Home button
+	    case android.R.id.home:
+	        NavUtils.navigateUpFromSameTask(this);
+	        return true;
+	    }
+	    return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		getActionBar().setTitle(R.string.preferences);
+	}
+	
+}
