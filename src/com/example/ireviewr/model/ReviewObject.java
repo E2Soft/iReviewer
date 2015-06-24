a39f6261def7e8b0ca54d3e493d1a2a5eed3d83c
zeljko.bal <zeljko.bal@gmail.com>
2015-06-24 23:58:31 +0200

    - metode za filtriranje ReviewObject (po tagovima i lokaciji) i Review (po tagovima i trenutnom korisniku)

--- src/com/example/ireviewr/model/ReviewObject.java
+++ src/com/example/ireviewr/model/ReviewObject.java
@@ -2,6 +2,7 @@ package com.example.ireviewr.model;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import com.activeandroid.annotation.Column;
@@ -9,6 +10,7 @@ import com.activeandroid.annotation.Column.ForeignKeyAction;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.From;
 import com.activeandroid.query.Select;
+import com.example.ireviewr.tools.ReviewerTools;
 
 
 @Table(name = "ReviewObject", id="_id")
@@ -99,7 +101,7 @@ public class ReviewObject extends AbstractModel
         return getMany(Review.class, "reviewObject");
     }
 	
-	public List<Review> getReviewsByTags(List<Tag> tags)
+	public List<Review> getReviewsFilteredByTags(List<Tag> tags)
 	{
 		From query = new Select()
 		.from(Review.class).as("r");
@@ -116,6 +118,42 @@ public class ReviewObject extends AbstractModel
 		
 		return query.execute();
     }
+	
+	public List<Review> filterByUser(List<Review> toFilter, User user)
+	{
+		Iterator<Review> it = toFilter.iterator();
+		while(it.hasNext())
+		{
+			Review review = it.next();
+			if(review.getGroups().size() == 0) continue; // ako nije u grupi preskoci
+			
+			boolean isInGroup = false;
+			for(Group group : user.getGroupsMember())
+			{
+				if(review.isInGroup(group.getModelId())) // ako je u istoj grupi kao user ok
+				{
+					isInGroup = true;
+					break;
+				}
+			}
+			
+			if(!isInGroup) it.remove(); // ako nije izbaci
+		}
+		
+		return toFilter;
+	}
+	
+	/**
+	 * Vraca reviewove filtrirane na osnovu zadatih parametara.
+	 * @param tags tagovi zakaceni za review
+	 * @param user trenutni korisnik
+	 * @return lista reviewova
+	 */
+	public List<Review> getFilteredReviews(List<Tag> tags, User user)
+	{
+		List<Review> resultSet = getReviewsFilteredByTags(tags);
+		return filterByUser(resultSet, user);
+	}
 	
 	public void addReview(Image toAdd)
 	{
@@ -171,7 +209,7 @@ public class ReviewObject extends AbstractModel
 		toAdd.save();
 	}
 	
-	public static List<ReviewObject> filterByTags(List<Tag> tags)
+	public static List<ReviewObject> getFilteredByTags(List<Tag> tags)
 	{
 		From query = new Select()
 		.from(ReviewObject.class).as("r");
@@ -185,6 +223,35 @@ public class ReviewObject extends AbstractModel
 		}
 		
 		return query.execute();
+	}
+	
+	public static List<ReviewObject> filterByDistance(List<ReviewObject> toFilter, double latitude, double longitude, double distance)
+	{
+		Iterator<ReviewObject> it = toFilter.iterator();
+		while(it.hasNext())
+		{
+			ReviewObject revob = it.next();
+			if(ReviewerTools.haversineDistance(latitude, longitude, 
+					revob.getLocationLat(), revob.getLocationLong()) > distance) // ako je udaljen vise od distance
+			{
+				it.remove(); // izbaci ga
+			}
+		}
+		return toFilter;
+	}
+	
+	/**
+	 * Vraca ReviewObject iz baze filtrirane po zadatim parametrima.
+	 * @param tags lista tagova po kojima se filtrira
+	 * @param latitude koordinata zadate tacke
+	 * @param longitude koordinata zadate tacke
+	 * @param distance udaljenost od zadate tacke
+	 * @return lista ReviewObject
+	 */
+	public static List<ReviewObject> getFiltered(List<Tag> tags, double latitude, double longitude, double distance)
+	{
+		List<ReviewObject> resultSet = getFilteredByTags(tags);
+		return filterByDistance(resultSet, latitude, longitude, distance);
 	}
 
 	public float getAverageRating()
--- src/com/example/ireviewr/tools/ReviewerTools.java
+++ src/com/example/ireviewr/tools/ReviewerTools.java
@@ -8,6 +8,11 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.atan2;
+import static java.lang.Math.PI;
 
 public class ReviewerTools {
 
@@ -73,5 +78,20 @@ public class ReviewerTools {
 			tagsString.append(" #").append(tag);
 		}
 		return tagsString.toString();
+	}
+	
+	public static double haversineDistance(double lat1, double lon1, double lat2, double lon2)
+	{
+		double R = 6371;
+		double dLat = (lat2 - lat1) * PI / 180;
+		double dLon = (lon2 - lon1) * PI / 180;
+		lat1 = lat1 * PI / 180;
+		lat2 = lat2 * PI / 180;
+		
+		double a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2);
+		double c = 2 * atan2(sqrt(a), sqrt(1-a));
+		double d = R * c;
+		
+		return d;
 	}
 }
