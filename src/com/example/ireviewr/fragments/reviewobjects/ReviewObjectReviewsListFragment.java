52ec2c44f95301749136f2dde983f9cbada06aff
zeljko.bal <zeljko.bal@gmail.com>
2015-06-25 19:05:21 +0200

    - filtriranje liste reviewova u detaljima za review object

--- src/com/example/ireviewr/fragments/reviewobjects/ReviewObjectReviewsListFragment.java
+++ src/com/example/ireviewr/fragments/reviewobjects/ReviewObjectReviewsListFragment.java
@@ -1,23 +1,33 @@
 package com.example.ireviewr.fragments.reviewobjects;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
 import com.example.ireviewr.R;
 import com.example.ireviewr.fragments.reviews.AbstractReviewsListFragment;
 import com.example.ireviewr.loaders.ModelLoaderCallbacks;
 import com.example.ireviewr.model.Review;
 import com.example.ireviewr.model.ReviewObject;
+import com.example.ireviewr.model.Tag;
+import com.example.ireviewr.tools.CurrentUser;
 
 public class ReviewObjectReviewsListFragment extends AbstractReviewsListFragment
 {
+	private List<Tag> tagFilter; // TODO popuniti tagFilter, issue #22
+	
 	public ReviewObjectReviewsListFragment()
-	{}
+	{
+		tagFilter = new ArrayList<Tag>();
+	}
 	
 	public ReviewObjectReviewsListFragment(String itemId)
 	{
 		super(itemId, R.menu.standard_list_menu);
+		tagFilter = new ArrayList<Tag>();
 	}
 	
 	@Override
@@ -28,7 +38,7 @@ public class ReviewObjectReviewsListFragment extends AbstractReviewsListFragment
 			@Override
 			protected List<Review> getData()
 			{
-				return getRevob().getReviews();
+				return getRevob().getFilteredReviews(tagFilter, CurrentUser.getModel(context));
 			}
 		};
 	}
